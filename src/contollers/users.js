4b1337f12926f61f46b5776ce4d2e253c7238a49
Mohammed Shoman <mohammedshoman9@gmail.com>
2022-07-18 00:44:15 +0300

    finish all crud conrollers

--- src/contollers/users.js
+++ src/contollers/users.js
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 import Users from '../models/users.js';
 
+/**
+ * find => returns an array
+ * findOne => returns a single object
+ */
+
 export const getAllUsers = async (req, res) => {
   try {
     const allUsers = await Users.find();
@@ -11,23 +16,62 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-export const getSingleUser = (req, res) => {
-  res.json('hello man');
-};
-export const createNewUser = (req, res) => {
-  const { name, age, rule } = req.body;
 
-  const newUser = new Users({
-    name,
-    age,
-    rule,
-  });
-  newUser.save();
-  res.status(201).json(newUser);
+export const getSingleUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const query = { _id: userId }; // _id comes from DB by default (automated feild)
+    // const query = { name }; // name comes from req.body and it's exist in model
+    // const query = { name: userName }; // if userName varible name is different
+    // const selectedUser = await Users.find(query); // this will return array
+    // const selectedUser = await Users.findOne(query); // this will return object
+    const selectedUser = await Users.findById(userId); // this will return object
+
+    res.status(200).json(selectedUser);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
-export const updateUser = (req, res) => {
-  res.json('hello man');
+
+export const createNewUser = async (req, res) => {
+  // const userObject = req.body; // {name: userObject.name, ...}
+  // const userObject = req.body; // copy by referrence // {...userObject}
+  // const {...rest} = req.body; // copy by value // {...rest}
+  // const {name, age, rule} = req.body; // {name, age, rule}
+  // const ahmed = {...req.body, name: "mohammed" } // this will override name
+  const userObject = req.body;
+  try {
+    // const newUser = new Users({
+    //   ...userObject,
+    // });d
+    // newUser.save(); // old way
+    const newUser = await Users.create(userObject); // new way
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
-export const deleteUser = (req, res) => {
-  res.json('hello man');
+
+export const updateUser = async (req, res) => {
+  const userObject = req.body;
+  const userId = req.params.id;
+  try {
+    // const selectedUser = await Users.findOneAndUpdate({ id: userId }, { ...userObject });
+    const selectedUser = await Users.findByIdAndUpdate(userId, { ...userObject });
+
+    res.status(200).json(selectedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const deletedUser = await Users.findByIdAndDelete(userId);
+
+    res.status(200).json({ message: `${deletedUser.name} is deleted succefully` });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
